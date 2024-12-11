945862648d73167677fb0d160cc8de2c9bd1a277
unknown <sw091319@gmail.com>
2024-12-11 15:17:11 -0500

    Add Wheatpack and Beatpack in shop

--- game.js
+++ game.js
@@ -1,11 +1,11 @@
 import { drawGrass } from './grass.js';
-import { move, drawCharacter, growSeeds } from './character.js';
+import { move, drawCharacter, growSeeds, character } from './character.js';
 import { drawGround } from './ground.js';
-import { drawInventory } from './inventory.js';
+import { drawInventory, inventory } from './inventory.js';
 import { drawCrop } from './crops.js';
 import { keyboardActions } from './keyboard.js';
 import { drawUI, ingameUiLocation } from './ui.js';
-import { shop, drawShop } from './shop.js';
+import { shopIcon, drawShop } from './shop.js';
 
 const canvas = document.getElementById('game');
 const ctx = canvas.getContext('2d');
@@ -34,18 +34,33 @@ window.addEventListener('keyup', (e) => {
 });
 
 let isShopClicked = false;
-let shopOpen = false;
+let iswheatbuy = false;
+let isbeatbuy = false;
+const shopDoc = document.getElementById('shop');
+
 canvas.addEventListener('mousedown', (e) => {
     const x = e.clientX - canvas.getBoundingClientRect().left;
     const y = e.clientY - canvas.getBoundingClientRect().top;
     if (x > ingameUiLocation(canvas).shop_ui[0] && x < ingameUiLocation(canvas).shop_ui[0] + ingameUiLocation(canvas).shop_ui[2] && y > ingameUiLocation(canvas).shop_ui[1] && y < ingameUiLocation(canvas).shop_ui[1] + ingameUiLocation(canvas).shop_ui[3]) {
-        isShopClicked = true;
-        shopOpen = true;
+        isShopClicked = !isShopClicked;
+    }
+    if (x> 480 && x < 480 + shopIcon.buyUnclick[2] && y > 234 && y < 234 + shopIcon.buyUnclick[3]*2) {
+        iswheatbuy = true;
+        character.coin -= 1;
+        inventory.wheat += 1;
+    }
+    if (x> 480 && x < 480 + shopIcon.buyUnclick[2] && y > 234 + 253-95 && y < 234 + 253-95 + shopIcon.buyUnclick[3]*2) {
+        isbeatbuy = true;
+        character.coin -= 2;
+        inventory.beat += 1;
     }
 });
+
 canvas.addEventListener('mouseup', (e) => {
-    isShopClicked = false;
+    iswheatbuy = false;
+    isbeatbuy = false;
 });
+
 
 function init() {
     resize();
@@ -60,15 +75,32 @@ function drawGame() {
     drawGround({ctx});
     drawInventory({ctx, canvas});
     drawUI({ctx});
-    //drawShop({ctx, canvas});
-    if(isShopClicked) {
-        ctx.drawImage(shop.img, ...shop.shopClick, 10 + ingameUiLocation(canvas).coin_ui[2] + 10, 12, (795-772)*3, (124-99)*3);
-    }
-    else {
-        ctx.drawImage(shop.img, ...shop.shopUnclick, 10 + ingameUiLocation(canvas).coin_ui[2] + 10, 10, (795-772)*3, (124-99)*3);
-    }
     drawCrop({ctx});
     drawCharacter({ctx});
+    if(isShopClicked) {
+        ctx.drawImage(shopIcon.img, ...shopIcon.shopClick, 10 + ingameUiLocation(canvas).coin_ui[2] + 10, 12, (795-772)*3, (124-99)*3);
+        drawShop({ctx, canvas});
+        if (iswheatbuy) {
+            ctx.drawImage(shopIcon.img, ...shopIcon.buyClick, 480, 234, shopIcon.buyClick[2], shopIcon.buyClick[3]*2);
+        }
+        else{
+            ctx.drawImage(shopIcon.img, ...shopIcon.buyUnclick, 480, 234, shopIcon.buyUnclick[2], shopIcon.buyUnclick[3]*2);
+        }
+        if (isbeatbuy) {
+            ctx.drawImage(shopIcon.img, ...shopIcon.buyClick, 480, 234 + 253-95, shopIcon.buyClick[2], shopIcon.buyClick[3]*2);
+        }
+        else{
+            ctx.drawImage(shopIcon.img, ...shopIcon.buyUnclick, 480, 234 + 253-95, shopIcon.buyUnclick[2], shopIcon.buyUnclick[3]*2);
+        }
+        ctx.font = "30px uifont";
+        ctx.fillStyle = "rgba(0, 0, 0, 1)";
+        ctx.fillText("Buy", 480 + 23, 234 + 39);
+        ctx.fillText("Buy", 480 + 23, 234 + 39 + 253-95);
+
+    }
+    else {
+        ctx.drawImage(shopIcon.img, ...shopIcon.shopUnclick, 10 + ingameUiLocation(canvas).coin_ui[2] + 10, 10, (795-772)*3, (124-99)*3);
+    }
 }
 
 export const getLastWidthIndex = () => Math.floor((canvas.width - 88) / 68);
--- shop.js
+++ shop.js
@@ -1,16 +1,59 @@
-import { inventory } from "./inventory.js";
+import { crop } from "./crops.js";
 import { ingameUiLocation } from "./ui.js";
 
 const shopimg = new Image();
 shopimg.src = './img/Sprite sheets/Sprite sheet for Basic Pack.png';
-export const shop = {
+export const shopIcon = {
     img: shopimg,
     shopUnclick: [772, 99, 795-772, 124-99],
-    shopClick: [804, 101, 827-804, 124-101]
+    shopClick: [804, 101, 827-804, 124-101],
+    coin : [627, 97, 636 - 627, 110 - 97],
+    buyUnclick: [162, 177, 253-162, 205-177],
+    buyClick: [259, 179, 349-259, 205 - 179]
+}
+
+const shopBackgroundimg = new Image();
+shopBackgroundimg.src = './img/Sprite sheets/Setting menu.png';
+const shopDialogimg = new Image();
+shopDialogimg.src = './img/Sprite sheets/Dialogue UI/Premade dialog box small.png';
+const shopEmojiimg = new Image();
+shopEmojiimg.src = './img/Sprite sheets/Dialogue UI/Emotes/Teemo Basic emote animations sprite sheet.png';
+
+export const shopBackground = {
+    backgroundimg: shopBackgroundimg,
+    background: [138, 12, 245-138, 134-12],
+    dialogimg: shopDialogimg,
+    dialog: [2, 1, 173-2, 62-1],
+    emojiimg: shopEmojiimg,
+    emoji: [4, 33, 28-4, 64-33]
+
 }
 
 export function drawShop({ctx, canvas}) {
-    ctx.drawImage(shop.img, ...shop.shopUnclick, 10 + ingameUiLocation(canvas).coin_ui[2] + 10, 10, (795-772)*3, (124-99)*3);
-    //ctx.drawImage(shop.img, ...shop.shopClick, 10 + ingameUiLocation(canvas).coin_ui[2] + 10, 12, (795-772)*3, (124-99)*3);
+    ctx.drawImage(shopBackground.backgroundimg, ...shopBackground.background, ingameUiLocation(canvas).shop_ui[0], ingameUiLocation(canvas).shop_ui[1] + ingameUiLocation(canvas).shop_ui[3] + 20 , shopBackground.background[2]*4, shopBackground.background[3]*4.5);
+
+    //Shop Text
+    ctx.font = "40px uifont";
+    ctx.fillStyle = "rgba(0, 0, 0, 1)";
+    ctx.fillText("Shop", ingameUiLocation(canvas).shop_ui[0] + 175, ingameUiLocation(canvas).shop_ui[1] + ingameUiLocation(canvas).shop_ui[3] + 80);
+
+    //wheatPack
+    ctx.drawImage(shopBackground.dialogimg, ...shopBackground.dialog, ingameUiLocation(canvas).shop_ui[0] + 15, ingameUiLocation(canvas).shop_ui[1] + ingameUiLocation(canvas).shop_ui[3] + 95, shopBackground.dialog[2]*2.35, shopBackground.dialog[3]*2.35);
+    ctx.drawImage(crop.img, ...crop.wheat_pack, ingameUiLocation(canvas).shop_ui[0] + 51, 214, crop.wheat_pack[2]*5, crop.wheat_pack[3]*5);
+    ctx.drawImage(shopIcon.img, ...shopIcon.coin, 350, 242, shopIcon.coin[2]*3, shopIcon.coin[3]*3);
+    ctx.fillText(1, 350 + 40, 242 + 35)
+
+    //beatPack
+    ctx.drawImage(shopBackground.dialogimg, ...shopBackground.dialog, ingameUiLocation(canvas).shop_ui[0] + 15, ingameUiLocation(canvas).shop_ui[1] + ingameUiLocation(canvas).shop_ui[3] + 253, shopBackground.dialog[2]*2.35, shopBackground.dialog[3]*2.35);
+    ctx.drawImage(crop.img, ...crop.beat_pack, ingameUiLocation(canvas).shop_ui[0] + 51, 214 + 253-95, crop.beat_pack[2]*5, crop.beat_pack[3]*5);
+    ctx.drawImage(shopIcon.img, ...shopIcon.coin, 350, 242 + 253-95, shopIcon.coin[2]*3, shopIcon.coin[3]*3);
+    ctx.fillText(2, 350 + 40, 242 + 253-95 + 35)
+
+    //Dialog
+    ctx.drawImage(shopBackground.dialogimg, ...shopBackground.dialog, ingameUiLocation(canvas).shop_ui[0] + 15, 495, shopBackground.dialog[2]*2.35, shopBackground.dialog[3]*2.35);
+    ctx.drawImage(shopBackground.emojiimg, ...shopBackground.emoji, ingameUiLocation(canvas).shop_ui[0] + 15 + 13*2.35, 495 + 13*2.35, shopBackground.emoji[2]*3.2, shopBackground.emoji[3]*2.5);
     ctx.font = "30px uifont";
+    ctx.fillText("Comming Soon...", 360, 590)
+
+    
 }
